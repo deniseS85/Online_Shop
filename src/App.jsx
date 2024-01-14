77e6a09c712159f0e130a15ece17bb957a204bdd
deniseS85 <denisabel78@hotmail.de>
2024-01-14 14:08:54 +0100

    finished online-shop

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,51 @@ import Navbar from './components/navbar';
 import ShoppingCart from './components/shopping-cart';
 
 class App extends Component {
-    state = {  } 
+    state = { 
+        items: []
+     } 
+
+    addItem = (name, price) => {
+        let currentItems = this.state.items;
+        let isExistingItems = this.state.items.find(item => item.name === name);
+
+        if (isExistingItems) {
+            isExistingItems.amount++;
+        } else {
+            currentItems.push({
+                amount: 1, 
+                name, 
+                price
+            });
+        }
+        this.setState({items: currentItems});
+    }
+
     render() { 
-        return (<React.Fragment>
-                    <Navbar/>
-                    <div className='main-container'>
-                        <div className='product-container'>
-                            <Product image="tomato.jpg" title="Tomaten" description="Füge Tomaten in deinem Warenkorb hinzu"/>
-                            <Product image="cucumber.jpg" title="Gurken" description="Füge Gurken in deinem Warenkorb hinzu"/>
-                            <Product image="paprika.jpg" title="Paprika" description="Füge Paprika in deinem Warenkorb hinzu"/>
-                            <Product image="zucchini.jpg" title="Zucchini" description="Füge Zucchini in deinem Warenkorb hinzu"/>
-                            <Product image="eggplant.jpg" title="Auberginen" description="Füge Aubergine in deinem Warenkorb hinzu"/>
-                        </div>
-                        <ShoppingCart/>
+        const products = [
+            {name: 'Tomaten', price: 2.99, image: 'tomato.jpg', description: 'Füge Tomaten in deinem Warenkorb hinzu'},
+            {name: 'Gurken', price: 1.99, image: 'cucumber.jpg', description: 'Füge Gurken in deinem Warenkorb hinzu'},
+            {name: 'Paprika', price: 3.99, image: 'paprika.jpg', description: 'Füge Paprika in deinem Warenkorb hinzu'},
+            {name: 'Zucchini', price: 3.59, image: 'zucchini.jpg', description: 'Füge Zucchini in deinem Warenkorb hinzu'},
+            {name: 'Auberginen', price: 3.50, image: 'eggplant.jpg', description: 'Füge Auberginen in deinem Warenkorb hinzu'},
+        ]
+        return (
+            <React.Fragment>
+                <Navbar/>
+                <div className='main-container'>
+                    <div className='product-container'>
+                        {products.map(product => (
+                            <Product key={product.name}
+                            onAdd={() => this.addItem(product.name, product.price)}
+                            image={product.image}
+                            title={product.name}
+                            description={product.description}
+                            />
+                        ))}
                     </div>
-                    
-                </React.Fragment>);
+                    <ShoppingCart items={this.state.items}/>
+                </div>
+            </React.Fragment>);
     }
 }
  
--- src/components/shopping-cart.jsx
+++ src/components/shopping-cart.jsx
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
-    state = { 
-        items: [
-            {
-                amount: 1,
-                name: 'Tomaten',
-                price: 4.9
-            },
-            {
-                amount: 1,
-                name: 'Gurken',
-                price: 2.5
-            }
-        ]
-    } 
+    state = { } 
+
+    calculateTotal = () => {
+        const total = this.props.items.reduce((acc, item) => {
+            return acc + (item.amount * item.price);
+        }, 0);
+
+        return total.toFixed(2);
+    }
 
     render() { 
-        return (<div className='shopping-cart'>
+        return (
+            <div className='shopping-cart'>
+                <div className='sticky'>
                     <h2>Warenkorb</h2>
-                    {this.state.items.map(item => <div key={item.name}>{item.amount} {item.name} {item.price}</div>)}
-                </div>);
+                    {this.props.items.map(item =>  (
+                        <div className='single-product' key={item.name}>
+                            <div>{item.amount}</div> 
+                            <div>{item.name}</div> 
+                            <div>{item.price} €</div>
+                        </div>
+                    ))}
+                    <div className='total'>
+                        <strong>Total</strong>
+                        <strong>{this.calculateTotal()} €</strong>
+                    </div>
+                </div>
+            </div>
+            );
     }
 }
  
